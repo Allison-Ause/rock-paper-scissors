21a6ebf4e708e94dfea7bc7c3d4a537082af3098
Allison Ause <allison.ause@gmail.com>
2022-06-07 09:30:34 -0700

    created html, css, java and utils. Confirmed via console log that scoreGame and getRandom functions are successfully operating

--- app.js
+++ app.js
@@ -1,10 +1,52 @@
 // import needed modules
+import { getRandomItem, score } from './utils.js';
 
 // state
+const throws = ['rock', 'paper', 'scissors'];
+let wins = 0;
+let losses = 0;
+let total = 0;
 
+//these are global variables:
+let computerThrow = '';
+let userThrow = '';
+
+function scoreGame() {
+    computerThrow = getRandomItem(throws);
+
+    const result = score(userThrow, computerThrow);
+
+    if (result === 1) {
+        wins++;
+    }
+    else if (result === -1) {
+        losses++;
+    }
+    total++;
+    console.log(wins, losses, total);
+}
 // components
     // component
     // define and grab DOM elements
+const rockButton = document.getElementById('rock-select');
+const paperButton = document.getElementById('paper-select');
+const scissorsButton = document.getElementById('scissors-select');
+
+rockButton.addEventListener('click', () => {
+    userThrow = 'rock';
+    scoreGame();
+});
+
+paperButton.addEventListener('click', () => {
+    userThrow = 'paper';
+    scoreGame();
+});
+
+scissorsButton.addEventListener('click', () => {
+    userThrow = 'scissors';
+    scoreGame();
+});
+
     // display functions
     // optional: subscribe to events
         // event handlers - what needs to happen?
--- test/tests.js
+++ test/tests.js
@@ -1,18 +1,38 @@
 // IMPORT code to test here
 // import { fn } from '../module.js';
+import { score } from './utils.js';
 
 const test = QUnit.test;
 
 test('test your pure functions...', (expect) => {
-    //Arrange
+    //Arrange - 1
     // Set up your arguments and expectations
     const expected = true;
 
-    //Act 
+    //Act - 2
     // Call the function you're testing and set the result to a const
     const actual = true;
 
-    //Expect
+    //Expect - 3
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+
+
+test('scores winning throws as 1', (expect) => {
+    expect.equal(score('rock', 'scissors'), 1);
+//  expect.equal(score('scissors', 'paper'), 1);
+//  expect.equal(score('paper', 'rocks'), 1);
+});
+
+//test('scores tied/draw throws as 0', (expect) => {
+//    expect.equal(score('rock', 'rock'), 0);
+//    expect.equal(score('scissors', 'scissors'), 0); 
+//    expect.equal(score('paper', 'paper'), 0); 
+//});
+
+//test('scores losing throws as -1', (expect) => {
+//    expect.equal(score('rock', 'paper'), -1);
+//    expect.equal(score('paper', 'scissors'), 0); 
+//    expect.equal(score('scissors', 'rock'), 0); 
+//});
